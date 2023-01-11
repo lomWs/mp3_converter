bcc6f666aaa30632dbfe54e8580a23d19ec4452f
Roberto Scandale <96438278+lomWs@users.noreply.github.com>
2023-01-11 06:43:43 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,154 @@
+from mp3_Downloader import Mp3_Downloader as YTdownloader
+from tkinter import filedialog
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+#SET SOME PREFERENCES
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+######## APP CLASS ########
+
+class App(customtkinter.CTk):
+    
+    #### Costructor #### 
+    def __init__(self):
+        super().__init__()
+        self.download_path = "" 
+        # configure window
+        self.title("Mp3 Converter.py")
+        self.minsize(920,320)
+        self.maxsize(920,320)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((1, 2,3), weight=0)
+        
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=250,height=1000, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=7, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Mp3 Converter", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 20))
+        
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.download_btn_event,width=200,text="Download")
+        self.sidebar_button_1.grid(row=4, column=0, padx=20, pady=(40,10))
+        
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.directory_btn_event,width=200,text="Directory")
+        self.sidebar_button_2.grid(row=5, column=0, padx=20, pady=(0,52))
+        
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w",width=200)
+        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,width=200, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+        
+        # entry labels
+        self.entry_1 = customtkinter.CTkEntry(self, placeholder_text="1)Singer-Title",height=35,width=600)
+        self.entry_1.grid(row=1, column=1, columnspan=1, padx=(20, 20), pady=(0, 10), sticky="nsew")
+
+        self.entry_2 = customtkinter.CTkEntry(self, placeholder_text="2)Singer-Title",height=35)
+        self.entry_2.grid(row=2, column=1, columnspan=2, padx=(20, 20), pady=(0, 10), sticky="nsew")
+      
+        self.entry_3 = customtkinter.CTkEntry(self, placeholder_text="3)Singer-Title",height=35)
+        self.entry_3.grid(row=3, column=1, columnspan=2, padx=(20, 20), pady=(0, 10), sticky="nsew")
+     
+        self.entry_4 = customtkinter.CTkEntry(self, placeholder_text="4)Singer-Title",height=35)
+        self.entry_4.grid(row=4, column=1, columnspan=2, padx=(20, 20), pady=(0, 10), sticky="nsew")
+       
+        self.entry_5 = customtkinter.CTkEntry(self, placeholder_text="5)Singer-Title",height=35)
+        self.entry_5.grid(row=5, column=1, columnspan=2, padx=(20, 20), pady=(0, 10), sticky="nsew")
+        
+
+
+    #### Event method for appearance button #### 
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    #### Event method for download button #### 
+    def download_btn_event(self):
+        flags = [False]*5
+        
+        #maybe it doesn't the best way to check if the thread is started
+        if(self.entry_1.get() != "" ):
+            bot1 = YTdownloader(self.entry_1.get(),self.download_path)
+            bot1.start()
+            flags[0]=True
+        if(self.entry_2.get() != ""):
+            bot2 = YTdownloader(self.entry_2.get(),self.download_path)
+            bot2.start()
+            flags[1]=True
+        if(self.entry_3.get() != ""):
+            bot3 = YTdownloader(self.entry_3.get(),self.download_path)
+            bot3.start()
+            flags[2]=True
+        if(self.entry_4.get() != ""):
+            bot4 = YTdownloader(self.entry_4.get(),self.download_path)
+            bot4.start()
+            flags[3]=True
+        if(self.entry_5.get() != ""):
+            bot5 = YTdownloader(self.entry_5.get(),self.download_path)
+            bot5.start()
+            flags[4]=True
+        
+        #then if thread is started it reuses flags' list to check if download is completed
+        if flags[0]==True:
+            bot1.join()
+            if bot1.download_status():
+                flags[0]="DOWNLOADED"
+            else:
+                flags[0]="NO"
+        if flags[1]==True:
+            bot2.join()
+            if bot2.download_status():
+                flags[0]="DOWNLOADED"
+            else:
+                flags[0]="NO"
+        if flags[2]==True:
+            bot3.join() 
+            if bot3.download_status():
+                flags[0]="DOWNLOADED"
+            else:
+                flags[0]="NO"
+        if flags[3]==True:
+            bot4.join()
+            if bot4.download_status():
+                flags[0]="DOWNLOADED"
+            else:
+                flags[0]="NO"
+        if flags[4]==True:
+            bot5.join()
+            if bot5.download_status():
+                flags[0]="DOWNLOADED"
+            else:
+                flags[0]="NO"
+        
+        
+        if self.check_all_download(flags):
+            tkinter.messagebox.showinfo(title="Success", message="Downloads completed")
+        else:
+            tkinter.messagebox.showerror(title="Error", message="Something went wrong")
+            
+    
+    #### Check method for download #### 
+    def check_all_download(self,list):
+        for i in range(len(list)):
+            if list[i] ==  "NO":
+                return False
+        return True            
+
+            
+    #### Event method for directory button ####        
+    def directory_btn_event(self):   
+        folder_selected = filedialog.askdirectory()
+        
+        self.download_path = folder_selected.replace('/','\\')
+        print(self.download_path)
+
+
+## MAIN FUNCTION THAT "RUN APP" ##
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+    
---
+++ src/mp3_Downloader.py
@@ -0,0 +1,126 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import time
+import os.path
+from  threading import Thread
+
+#GLOBAL CONSTANT
+CHROME_DRIVER_PATH = "C:\Program Files (x86)\Selinium\chromedriver.exe"
+YOUTUBE_LINK = "https://www.youtube.com/results?search_query="
+CONVERTER_LINK = "https://notube.li/it/youtube-app-v20"
+
+
+######## DOWNLOADER CLASS ########
+class Mp3_Downloader(Thread):#using subclass
+
+    
+    #### Costructor #### 
+    def __init__(self,user_query,download_path):
+        #inizialize the thread
+        Thread.__init__(self)
+        #inizialize variables and options
+        self.user_query = user_query
+        
+        self.download_path = download_path
+        self.song = ""
+        options  = self.set_option()
+        self.driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+        
+        self.driver = webdriver.Chrome(options=options)
+        
+           
+    #### Web driver options setter ####   
+    def set_option(self):
+        options = Options()
+        
+        if self.download_path != "":#if user don't specify the path it takes the standard download path
+            #change download directory
+            prefs = {"download.default_directory" : self.download_path}#'C:\prova' path type
+            options.add_experimental_option("prefs", prefs)
+        
+        #hide windows
+        options.add_argument('--headless')
+        
+        return options
+    
+    
+    #### Get youtube video's link ####
+    def yt_link(self):
+        #go on "youtube + name of song"
+        self.driver.get(YOUTUBE_LINK+self.user_query)
+        self.driver.implicitly_wait(10)
+        #wait the page and click accept button
+        acbutton = WebDriverWait(self.driver , 15).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button')))
+        acbutton.click()
+        self.driver.implicitly_wait(10)
+        #save video's link
+        link = self.driver.find_element(By.XPATH,'//*[@id="video-title"]').get_attribute('href')
+        
+        #grab the name of the video
+        self.song = self.driver.find_element(By.XPATH,'//*[@id="video-title"]').get_attribute('title')
+        
+        return link
+    
+    #### Download the file in mp3 ####
+    def download_song(self):  
+        
+        #get link of the first video
+        link = self.yt_link()
+        
+        #open new tab with js script
+        self.driver.execute_script("window.open('');")
+        time.sleep(5)
+        
+        #switch to converter page
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.get(CONVERTER_LINK)
+        
+        #wait the page and paste the link
+        WebDriverWait(self.driver , 15).until(EC.element_to_be_clickable((By.ID,'keyword'))).send_keys(link)
+        WebDriverWait(self.driver , 15).until(EC.element_to_be_clickable((By.ID,'submit-button'))).click()
+        
+        #start downloading
+        WebDriverWait(self.driver , 17).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="downloadButton"]'))).click()
+        
+        #formatting song name to check the downoload status
+        self.song = self.song +".mp3"
+        
+        #close spam pages
+        self.close_up()
+        
+        #check download_status
+        while not self.download_status():   
+            time.sleep(1)
+        
+        #close session
+        self.driver.quit()
+        
+        
+    #### Check download status ####
+    def download_status(self):#if download is finished close the pages
+        if os.path.isfile(self.download_path +"\\" +self.song):
+            return True
+        else:
+            return False
+        
+    #### Close spam pages ####  
+    def close_up(self): #close unless page
+        handles = self.driver.window_handles
+        handles.remove(handles[1])#removing converter page and close others
+        for tab in handles:
+            self.driver.switch_to.window(tab)
+            
+            time.sleep(1)
+            self.driver.close()
+            
+            
+    def run(self):#thread's method
+        #start download
+        self.download_song()
+                 
+######## /DOWNLOADER CLASS ########
+
+
